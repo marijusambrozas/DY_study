866d5911e548d7b80515a19c3deb91cdbdc5e915
marijusambrozas <marijus.ambrozas@gmail.com>
2020-01-23 11:03:54 +0200

    Added histogram merging

---
+++ BkgEst/FakeRate/MergeHistograms_C.d
@@ -0,0 +1,24 @@
+
+# DO NOT DELETE
+
+./MergeHistograms_C.so: /usr/include/root/TStyle.h /usr/include/root/TChain.h
+./MergeHistograms_C.so: /usr/include/root/TFile.h /usr/include/root/TH1D.h
+./MergeHistograms_C.so: /usr/include/root/TGraphAsymmErrors.h
+./MergeHistograms_C.so: /usr/include/root/TMultiGraph.h
+./MergeHistograms_C.so: /usr/include/root/TLegend.h
+./MergeHistograms_C.so: /usr/include/root/TCanvas.h /usr/include/root/TROOT.h
+./MergeHistograms_C.so: /usr/include/root/THStack.h /usr/include/root/TMath.h
+./MergeHistograms_C.so: /usr/include/root/TText.h /usr/include/root/TPad.h
+./MergeHistograms_C.so: /usr/include/root/TPaveText.h
+./MergeHistograms_C.so: /usr/include/root/TLorentzVector.h
+./MergeHistograms_C.so: /usr/include/root/TStopwatch.h
+./MergeHistograms_C.so: /usr/include/root/TColor.h /usr/include/root/TLatex.h
+./MergeHistograms_C.so: /usr/include/root/TEfficiency.h
+./MergeHistograms_C.so: /usr/include/c++/4.8.5/iostream
+./MergeHistograms_C.so: /usr/include/c++/4.8.5/string
+./MergeHistograms_C.so: /usr/include/c++/4.8.5/fstream
+./MergeHistograms_C.so: /usr/include/c++/4.8.5/sstream
+./MergeHistograms_C.so: /usr/include/c++/4.8.5/iomanip
+./MergeHistograms_C.so: /usr/include/c++/4.8.5/vector
+./MergeHistograms_C.so: /usr/include/root/RVersion.h /usr/include/root/RConfig.h /usr/include/root/TClass.h /usr/include/root/TDictAttributeMap.h /usr/include/root/TInterpreter.h /usr/include/root/TROOT.h /usr/include/root/TBuffer.h /usr/include/root/TMemberInspector.h /usr/include/root/TError.h /usr/include/root/RtypesImp.h /usr/include/root/TIsAProxy.h /usr/include/root/TFileMergeInfo.h /usr/include/root/TCollectionProxyInfo.h /usr/bin/rootcling
+MergeHistograms_C__ROOTBUILDVERSION= 6.08/06
